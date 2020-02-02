76db69f84125672426476cb3f261b609c26fe267
poifull10 <poifull10@gmail.com>
2020-02-02 14:48:26 +0900

    test pyscript

---
+++ tools/test_open3d.py
@@ -0,0 +1,26 @@
+import numpy as np
+import open3d as o3d
+
+src = np.loadtxt('data/src.csv')
+dst = np.loadtxt('data/dst.csv')
+
+print(src)
+print(dst)
+
+src_pcd = o3d.geometry.PointCloud()
+src_pcd.points = o3d.utility.Vector3dVector(src)
+dst_pcd = o3d.geometry.PointCloud()
+dst_pcd.points = o3d.utility.Vector3dVector(dst)
+
+src_pcd.paint_uniform_color([1, 0, 0])
+dst_pcd.paint_uniform_color([0, 0, 1])
+
+corres = o3d.utility.Vector2iVector()
+for i in range(len(src)):
+    corres.append(np.array([i, i]))
+
+TE = o3d.registration.TransformationEstimationPointToPoint()
+T = TE.compute_transformation(src_pcd, dst_pcd, corres)
+print(T)
+
+o3d.visualization.draw_geometries([src_pcd.transform(T), dst_pcd])
